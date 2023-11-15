b4cc5e9932caf3e695e6da14c054a646dc0c69b4
dgh06175 <dgh06175@gmail.com>
2023-11-15 19:34:14 +0900

    refactor: 인터페이스 삭제, 추상클래스로 통합

--- src/main/java/christmas/domain/events/Event.java
+++ src/main/java/christmas/domain/events/Event.java
@@ -1,9 +1,45 @@
 package christmas.domain.events;
 
-import christmas.domain.Date;
 import christmas.domain.OrderedMenu;
+import java.time.LocalDate;
 
-public interface Event {
-    int getDiscountOf(Date date, OrderedMenu orderedMenu);
-    String getName();
+public abstract class Event {
+    private static final int EVENT_MIN_PRICE = 10_000;
+    private final String name;
+    private final LocalDate startDay;
+    private final LocalDate endDay;
+
+    Event(LocalDate eventDate, String name, int startDay, int endDay) {
+        this.name = name;
+        this.startDay = LocalDate.of(eventDate.getYear(), eventDate.getMonth(), startDay);
+        this.endDay = LocalDate.of(eventDate.getYear(), eventDate.getMonth(), endDay);
+    }
+
+    public int getDiscountOf(LocalDate visitDate, OrderedMenu orderedMenu) {
+        if (!isEventValid(visitDate, orderedMenu)) {
+            return 0;
+        }
+        return calculateDiscount(visitDate, orderedMenu);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private boolean isEventValid(LocalDate visitDate, OrderedMenu orderedMenu) {
+        return isDateIncludeInEventDays(visitDate) && isTotalPriceValid(orderedMenu) && isSpecificEventValid(visitDate, orderedMenu);
+    }
+
+    private boolean isDateIncludeInEventDays(LocalDate visitDate) {
+        return visitDate.isAfter(startDay) || visitDate.isEqual(startDay) &&
+        (visitDate.isBefore(endDay) || visitDate.isEqual(endDay));
+    }
+
+    private boolean isTotalPriceValid(OrderedMenu orderedMenu) {
+        return orderedMenu.getTotalPrice() >= EVENT_MIN_PRICE;
+    }
+
+    protected abstract int calculateDiscount(LocalDate visitDate, OrderedMenu orderedMenu);
+
+    protected abstract boolean isSpecificEventValid(LocalDate visitDate, OrderedMenu orderedMenu);
 }
